2999853ae6383fc44b78f8d78bc4ae35c7e310c9
bbreuer <bbreuer445@gmail.com>
2024-04-09 19:15:38 -0400

    asadsdads

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const themeToggle = document.querySelector('.theme-controller');
-    // Select the SVG image element by its class or id
-    const logoImage = document.querySelector('img.h-8'); // Assuming 'h-8' is unique enough, otherwise add an id to your img tag and select it here
+    const logoImage = document.querySelector('img.h-8'); // Select the SVG image element
+    const envelopeIcon = document.getElementById('envelope-icon'); // Select the envelope image by its ID
 
     // Function to switch themes and save the current theme to localStorage
     function switchTheme() {
@@ -9,11 +9,13 @@ document.addEventListener('DOMContentLoaded', function () {
         if (currentTheme === 'cupcake') {
             document.body.setAttribute('data-theme', 'dark');
             localStorage.setItem('theme', 'dark'); // Save theme to localStorage
-            logoImage.src = "/computer-solid-white.svg"; // Change SVG to white version for dark theme
+            logoImage.src = "/computer-solid-white.svg"; // Change logo SVG to white version for dark theme
+            envelopeIcon.src = '/black-envelope.svg'; // Change envelope to black version for dark theme
         } else {
             document.body.setAttribute('data-theme', 'cupcake');
             localStorage.setItem('theme', 'cupcake'); // Save theme to localStorage
-            logoImage.src = "/computer-solid-black.svg"; // Change SVG to black version for cupcake theme
+            logoImage.src = "/computer-solid-black.svg"; // Change logo SVG to black version for cupcake theme
+            envelopeIcon.src = '/white-envelope.svg'; // Change envelope to white version for cupcake theme
         }
     }
 
@@ -25,8 +27,10 @@ document.addEventListener('DOMContentLoaded', function () {
             // Adjust the SVG source based on the saved theme
             if (savedTheme === 'dark') {
                 logoImage.src = "/computer-solid-white.svg";
+                envelopeIcon.src = '/white-envelope.svg'; // Set envelope to black for dark theme
             } else {
                 logoImage.src = "/computer-solid-black.svg";
+                envelopeIcon.src = '/black-envelope.svg'; // Set envelope to white for cupcake theme
             }
             // If using a toggle checkbox, adjust its 'checked' state based on the theme
             themeToggle.checked = savedTheme === 'dark';
