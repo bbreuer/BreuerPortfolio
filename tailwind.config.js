50ba215b9bf6af51c37bad1e9fce0c56182098f7
bbreuer <bbreuer445@gmail.com>
2024-04-12 11:58:04 -0400

    uwu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
+  themes:     {
+      dark: {                          // specifying the theme name to customize
+        "primary": "#a991f7",             // default primary color (you can change this if you want)
+        "secondary": "#f6d860",           // default secondary color
+        "accent": "#37cdbe",              // default accent color
+        "neutral": "#3d4451",             // default neutral color
+        "base-100": "#ffffff",            // default base color (background color of elements)
+        "info": "#3ABFF8",                // default info color
+        "success": "#36D399",             // default success color
+        "warning": "#FBBD23",             // default warning color
+        "error": "#F87272",               // default error color
+        "text": "#ffffff",                // changing text color to white
+      },
+    },
   plugins: [
     require('daisyui'),
   ],
